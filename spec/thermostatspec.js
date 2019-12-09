3c3a9b8e1b772d8a5f8a788e3db78bd4133a1a82
Lade Docemo <lade.doc@gmail.com>
2019-12-09 15:00:43 +0000

    The thermostat's temperature can now be increased

--- spec/thermostatspec.js
+++ spec/thermostatspec.js
@@ -5,8 +5,12 @@ describe("A Thermostat", function () {
         thermostat = new Thermostat();
     });
 
-    it("default temperature setting is 20 degrees", function(){
+    it("default temperature setting is 20 degrees", function () {
         expect(thermostat.temperature).toEqual(20);
     });
 
+    it("increases the temperature", function () {
+        thermostat.temp_up(32);
+        expect(thermostat.temperature).toEqual(32);
+    });
 });
